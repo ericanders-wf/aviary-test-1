cb8be4f19113c320b961dc1224467de644bea3c2
Eric Anders <eric.anders@workiva.com>
2016-11-07 16:35:22 -0600

    fix reqs

--- setup.py
+++ setup.py
@@ -2,15 +2,14 @@ from setuptools import setup
 from pip.req import parse_requirements
 
 
-setup(name='test-aviary',
-      version='1.0',
-      description='Test Aviary',
-      install_requires=get_requirements('requirements.txt')
-      )
-
 def get_requirements(filename):
     reqs = parse_requirements(filename)
 
     return [str(r.req) for r in reqs]
 
+setup(name='test-aviary',
+      version='1.0',
+      description='Test Aviary',
+      install_requires=get_requirements('requirements.txt')
+      )
 
