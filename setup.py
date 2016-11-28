4f1430ef95079eec4e3eea95d87e2ea37ea77d10
Eric Anders <eric.anders@workiva.com>
2016-11-28 10:17:11 -0600

    Reorder function defs in setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from distutils.core import setup
 from pip.req import parse_requirements
 
-setup(name='test-aviary',
-      version='1.0',
-      description='Test Aviary',
-      install_requires=get_requirements('requirements.txt')
-      )
 
 def get_requirements(filename):
     reqs = parse_requirements(filename, session=None)
 
     return [str(r.req) for r in reqs]
+
+
+setup(name='test-aviary',
+      version='1.0',
+      description='Test Aviary',
+      install_requires=get_requirements('requirements.txt')
+      )
 
